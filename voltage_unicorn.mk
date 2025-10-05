537b22c5e088f71735583f2bc2a6ce2897289b6a
dopaemon <polarisdp@gmail.com>
2025-10-05 04:14:10 +0000

    unicorn: VoltageOS BringUp
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- voltage_unicorn.mk
+++ voltage_unicorn.mk
@@ -7,10 +7,26 @@
 # Inherit from unicorn device
 $(call inherit-product, device/xiaomi/unicorn/device.mk)
 
-# Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from common voltage configuration
+$(call inherit-product, vendor/voltage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_unicorn
+# Voltage Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+VOLTAGE_BUILD_TYPE := UNOFFICIAL
+EXTRA_UDFPS_ANIMATIONS := true
+
+# Boost Framework
+VOLTAGE_CPU_SMALL_CORES := 0,1,2
+VOLTAGE_CPU_BIG_CORES   := 3,4,5,6,7
+VOLTAGE_ALL_CORES       := 0-7
+VOLTAGE_CPU_SYS_BG      := 0-3
+VOLTAGE_CPU_BG          := 0-2
+VOLTAGE_CPU_FG          := 0-5
+VOLTAGE_CPU_LIMIT_BG    := 0-1
+VOLTAGE_CPU_LIMIT_UI    := 0-2
+VOLTAGE_CPU_DISPLAY     := 0-5
+
+PRODUCT_NAME := voltage_unicorn
 PRODUCT_DEVICE := unicorn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
