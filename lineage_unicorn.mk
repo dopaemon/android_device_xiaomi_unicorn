312df9b09508eef3f649f29b208e9da7865acc81
dopaemon <polarisdp@gmail.com>
2025-10-23 10:50:52 +0000

    unicorn: BringUp AxionOS
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- lineage_unicorn.mk
+++ lineage_unicorn.mk
@@ -10,6 +10,23 @@ $(call inherit-product, device/xiaomi/unicorn/device.mk)
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# AxionOS
+TARGET_DISABLE_EPPE := true
+TARGET_ENABLE_BLUR := true
+AXION_CAMERA_REAR_INFO := 50,50,50
+AXION_CAMERA_FRONT_INFO := 32
+AXION_MAINTAINER := KernelPanix
+AXION_PROCESSOR := Snapdragon_8_Plus_Gen_1
+BYPASS_CHARGE_SUPPORTED ?= false
+HBM_SUPPORTED := false
+TARGET_INCLUDE_VIPERFX := true
+TORCH_STR_SUPPORTED := true
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.perf.scroll_opt = true \
+    persist.sys.perf.scroll_opt.heavy_app = 2
+
+
 PRODUCT_NAME := lineage_unicorn
 PRODUCT_DEVICE := unicorn
 PRODUCT_MANUFACTURER := Xiaomi
