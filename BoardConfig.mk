33730c8e0fe6883ed601875d8fbfd717687315e6
Arian <arian.kulmer@web.de>
2026-03-31 10:21:39 +0200

    unicorn: Update blobs and firmware from OS3.0.3.0.VLECNXM
    Change-Id: I72b5a5697907eb3e1f9e47fab76effe786b5c663

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 #
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2025-12-01
+VENDOR_SECURITY_PATCH := 2026-02-01
 
 # Inherit from xiaomi sm8450-common
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
